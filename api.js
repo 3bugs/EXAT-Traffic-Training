e57ce16a6ceaa855883bddfbc380187c6023c652
Promlert Lovichit <promlert@gmail.com>
2020-07-21 12:37:11 +0700

    fetch alg's api to update schematic maps

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const cronJob = require("cron").CronJob;
+const fetch = require('node-fetch');
 
 app.use(express.static('./'));
 
@@ -14,9 +15,20 @@ io.on('connection', function (socket) {
   });
 });
 
-new cronJob("*/1 * * * *", function() {
+new cronJob("*/1 * * * *", function () {
   console.log('CRON JOB RUN: ' + new Date());
-  io.emit('event-test', {msg: 'Hello Socket.IO'});
+  //io.emit('event-test', {msg: 'Hello Socket.IO'});
+
+  fetch('https://alg.exat.co.th/api/roads/1', {
+    method: 'get',
+    headers: {'Authorization': 'Token 8a4e96ed4c9281af4d0c2189c6a72551fe940b43'},
+  })
+    .then(result => result.json())
+    .then(result => {
+      const chunks = result[0].chunks;
+      console.log(chunks)
+      io.emit('update-traffic', chunks);
+    });
 }, null, true);
 
 http.listen(3000, function () {
